93fd146f276b05d991ff721cd1327f6d8a644fba
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2020-12-08 14:54:32 -0500

    First push to commit

---
+++ src/twoSum.scala
@@ -0,0 +1,18 @@
+object twoSum{
+  def main(args: Array[String]) {
+
+    var target = 22
+
+    var nums = Array(1, 2, 3, 5, 9, 13)
+
+    for(x <- nums) {
+      if(x <= target){
+        for(y <- nums){
+          if(x + y == target){
+            println(x + " + " + y + " = " + target)
+          }
+        }
+      }
+    }
+  }
+}
