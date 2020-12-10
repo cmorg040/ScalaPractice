7c373f1fa9646654538b096861ecf14573cb7b3e
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2020-12-09 21:45:00 -0500

    Updated file name

--- src/CSVreader.scala
+++ src/CSVreader.scala
@@ -1,5 +1,5 @@
 
-object followAlong extends App{
+object CSVreader extends App{
 
   val file = io.Source.fromFile("persons.csv")
 
@@ -11,10 +11,10 @@ object followAlong extends App{
     // line.split() will split a string into an array based on a delimiter.
     // E.G line.split("+") would split the string: 'Tim+40+Texas' into this array: [Tim, 40, Texas]
 
-    println(s"${values(0)}, ${values(1)}, ${values(2)}")
-    // This is just fancy string interpolation.
+    println(f"${values(0)}%-10s ${values(1)}%-10s ${values(2)}%-10s")
+    // This is just fancy formatting.
     // Same thing as this:
     // println(values(0) + ", " + values(1) + ", " + values(2))
-    // ^You can uncomment that line and it'll print the exact same thing.
+    // ^You can uncomment that line and it'll print the same shit without formatting(and with commas)
   }
 }
